60fbfd5532f99419366d4a91a4f9a54f2054d766
HauseMasterZ <113833707+HauseMasterZ@users.noreply.github.com>
2023-04-08 20:31:27 +0530

    Bug Fixes

---
+++ EditScreen.tsx
@@ -0,0 +1,24 @@
+import React, { useRef } from 'react';
+
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Modal, StatusBar, Button, Pressable } from 'react-native';
+import { Dimensions, Image } from "react-native";
+import RadioGroup, { RadioButtonProps } from 'react-native-radio-buttons-group';
+import { useState, } from 'react';
+
+
+
+
+
+
+const editScreen = (poses) =>{
+    console.log(poses)
+    return (
+        <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text>lmao ded </Text>
+
+        </View>
+    )
+}
+
+
+export default(editScreen)
