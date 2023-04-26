7b83837454b6713a331892237fa397b52cc8745d
HauseMasterZ <113833707+HauseMasterZ@users.noreply.github.com>
2023-04-26 20:26:54 +0530

    f

--- App.tsx
+++ App.tsx
@@ -1,97 +1,72 @@
-import React, { Component } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
-import TreeView from 'react-native-final-tree-view';
-import FinalTreeView from 'react-native-final-tree-view';
-import TreeCard from './Card';
-const data = [
+import React from 'react'
+import { Text, View } from 'react-native'
+
+import TreeView from 'react-native-final-tree-view'
+
+function getIndicator(isExpanded, hasChildrenNodes) {
+  isExpanded = true
+  if (!hasChildrenNodes) {
+    return '-'
+  } else if (isExpanded) {
+    return '\\/'
+  } else {
+    return '>'
+  }
+}
+const family = [
   {
-    id: 'grandparent',
-    name: 'Grandparent',
+    id: 'Grandparent',
+    name: 'Grandpa',
+    age: 78,
     children: [
       {
-        id: 'parent1',
-        name: 'Parent 1',
+        id: 'Me',
+        name: 'Me',
+        age: 30,
         children: [
           {
-            id: 'child1',
-            name: 'Child 1',
-            children: [],
+            id: 'Erick',
+            name: 'Erick',
+            age: 10,
           },
           {
-            id: 'child2',
-            name: 'Child 2',
-            children: [],
-          },
-        ],
-      },
-      {
-        id: 'parent2',
-        name: 'Parent 2',
-        children: [
-          {
-            id: 'child3',
-            name: 'Child 3',
-            children: [],
-          },
-          {
-            id: 'child4',
-            name: 'Child 4',
-            children: [],
+            id: 'Rose',
+            name: 'Rose',
+            age: 12,
           },
         ],
       },
     ],
   },
-];
-
-const Node = ({ node }) => {
+]
+function App() {
   return (
-    <View style={{ backgroundColor: '#1f6eed', width: 300, height: 300, padding: 20, borderRadius: 10 }}>
-      <Text style={{ color: 'white', fontSize: 20 }}>{node.name}</Text>
-    </View>
-  );
-};
+    <View style={{flex:1 , justifyContent:'center', alignItems:'center'}}>
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  isNodeExpanded = (node) => {
-    return !!this.state[node.id];
-  }
-
-  toggleNode = (node) => {
-    this.setState({ [node.id]: !this.isNodeExpanded(node) });
-  }
-
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', marginTop: 50 }}>
-        <TreeView
-          data={data}
-          renderNode={({ node, level, isExpanded, hasChildrenNodes }) => {
-            return (
-              <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 10 }}>
-                <View style={{ marginLeft: level * 20, marginRight: 10 }}>
-                  {hasChildrenNodes && (
-                    <TouchableOpacity onPress={() => this.toggleNode(node)}>
-                      <Text>{isExpanded ? '-' : '+'}</Text>
-                    </TouchableOpacity>
-                  )}
-                </View>
-                {/* <Node node={node} /> */}
-                <View style={{width: 200, height: 200}}>
-
-                {TreeCard()}
-                </View>
-              </View>
-            );
-          }}
-          isNodeExpanded={this.isNodeExpanded}
-        />
+    <TreeView
+      data={family} // defined above
+      initialExpanded = {true}
+      onNodePress={({node, level}) => {
+        console.log(node);
+        console.log(level);
+      }}
+      renderNode={({ node, level, isExpanded, hasChildrenNodes }) => {
+        return (
+          <View>
+            <Text
+              style={{
+                marginLeft: 25 * level,
+              }}
+            >
+              {isExpanded = true}
+              {getIndicator(isExpanded, hasChildrenNodes)} {node.name}
+            </Text>
+          </View>
+        )
+      }}
+      />
       </View>
-    );
-  }
+  )
 }
+// okol
+export default App
